c0165d46d657c2ebab01330c9ed519374ea93c6e
jigsaw <jigsaw@live.jp>
2016-06-22 01:34:30 +0900

    Little fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {Provider} from 'react-redux'
 import {applyMiddleware, createStore} from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import reducers from './reducers.js'
-import App from './containers//App.js'
+import App from './containers/App.js'
 const {createElement} = React
 
 const store = applyMiddleware(thunkMiddleware)(createStore)(reducers)
